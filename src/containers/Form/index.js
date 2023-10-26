4ddc8323fa55f80f7b4f24a7bd698492daab4f1b
Lucie <chimitslucie@gmail.com>
2023-10-26 07:28:08 +0200

    correction erreur test formulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,8 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+// modification du Timeour pour régler l'erreur du texte "Envoyer"
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 600); })
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
